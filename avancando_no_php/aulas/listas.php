d52b489b50c1249aca0b224df0212f2e6b75de85
gabrieldarlan <gabriel.darlan88@gmail.com>
2021-04-23 17:03:58 -0300

    curso avançando com php - cap 02 aula02 completa

---
+++ avancando_no_php/aulas/listas.php
@@ -0,0 +1,6 @@
+<?php
+
+$idadesList = [12, 32, 65, 84, 98, 36];
+$umaIdade = $idadesList[2];
+
+echo $umaIdade;
---
+++ avancando_no_php/aulas/loop-lista.php
@@ -0,0 +1,9 @@
+<?php
+
+$idadesList = [21, 23, 19, 25, 30, 41, 18];
+
+echo count($idadesList) . PHP_EOL;
+
+for ($i = 0; $i < count($idadesList); $i++) {
+    echo $idadesList[$i] . PHP_EOL;
+}
